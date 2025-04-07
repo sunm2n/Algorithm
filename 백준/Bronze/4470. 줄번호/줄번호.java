64af5f9dae51fadb643eea7315d08d8c6bff81b6
LEE SUN MIN <134378502+sunm2n@users.noreply.github.com>
2025-04-07 18:56:38 +0900

    [Bronze IV] Title: 줄번호, Time: 192 ms, Memory: 18492 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        sc.nextLine();
+        String []str = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            str[i] = sc.nextLine();
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.println((i + 1) + ". " + str[i]);
+        }
+
+    }
+}
