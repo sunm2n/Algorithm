8f771126f0943f886528d882ec3a85dac08b7cc7
LEE SUN MIN <134378502+sunm2n@users.noreply.github.com>
2025-04-14 17:25:44 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1676 ms, Memory: 191568 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,27 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            if(a[1] == b[1]) return a[0] - b[0];
+            return a[1] - b[1];
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
