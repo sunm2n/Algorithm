f3bd8b76ccdf489ac50d24d4b62c79310b56510b
LEE SUN MIN <134378502+sunm2n@users.noreply.github.com>
2025-04-20 12:14:25 +0900

    [Bronze V] Title: 웰컴, Time: 172 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
