381de30d0a239f19ccd8506a79430af6f9892a12
LEE SUN MIN <134378502+sunm2n@users.noreply.github.com>
2025-05-17 23:15:07 +0900

    [Silver IV] Title: 점화식, Time: 176 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        long[] t = new long[n+1];
+
+        t[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                t[i] += t[j] * t[i - 1 - j];
+            }
+        }
+
+        System.out.println(t[n]);
+    }
+}
