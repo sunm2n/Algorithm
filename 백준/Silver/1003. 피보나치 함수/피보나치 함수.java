c38aca4727b1529ace9cba1c1036a7369498da5f
LEE SUN MIN <134378502+sunm2n@users.noreply.github.com>
2025-05-27 14:11:00 +0900

    [Silver III] Title: 피보나치 함수, Time: 248 ms, Memory: 18504 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        //[i][0] => 0이 출력되는 횟수, [i][1] => 1이 출력되는 횟수
+        int [][]dp = new int[41][2]; 
+
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        // dp 테이블 미리 계산
+        for (int i = 2; i <= 40; i++) {
+            dp[i][0] = dp[i-1][0] + dp[i-2][0];
+            dp[i][1] = dp[i-1][1] + dp[i-2][1];
+        }
+
+        for (int i = 0; i < T; i++) {
+            int n = sc.nextInt();
+            System.out.println(dp[n][0] + " " + dp[n][1]);
+        }
+    }
+}
