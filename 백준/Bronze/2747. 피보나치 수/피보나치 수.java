0a296cf2742fa019428503b47a44b4c67994104d
LEE SUN MIN <134378502+sunm2n@users.noreply.github.com>
2025-06-25 09:39:24 +0900

    [Bronze II] Title: 피보나치 수, Time: 196 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int []dp = new int[n+1];
+
+        if(n==1) {
+            System.out.println(1);
+            return;
+        }
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
