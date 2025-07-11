19af3c8c4dc5c8efcbf9601645235bbca6606bfd
LEE SUN MIN <134378502+sunm2n@users.noreply.github.com>
2025-07-11 09:48:16 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 188 ms, Memory: 18236 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        System.out.println(":fan::fan::fan:\n" +
+                ":fan::"+ str + "::fan:\n" +
+                ":fan::fan::fan:");
+    }
+}
