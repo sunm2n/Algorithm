8271701681545f29e16c45cea982943db6630386
LEE SUN MIN <134378502+sunm2n@users.noreply.github.com>
2025-07-13 17:46:01 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 188 ms, Memory: 17508 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+
+        System.out.println(today.getYear());
+        System.out.printf("%02d\n", today.getMonthValue());
+        System.out.printf("%02d\n", today.getDayOfMonth());
+    }
+}
