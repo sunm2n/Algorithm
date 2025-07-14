65f4b9433af4b10044ec9998cf2c5ba6dc14ac1f
LEE SUN MIN <134378502+sunm2n@users.noreply.github.com>
2025-07-14 09:55:42 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 192 ms, Memory: 25416 KB, Score: 201 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int MOD = 1000000007;
+        long[] dp = new long[Math.max(2, n + 1)]; // 최소 크기 2로 확보
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
