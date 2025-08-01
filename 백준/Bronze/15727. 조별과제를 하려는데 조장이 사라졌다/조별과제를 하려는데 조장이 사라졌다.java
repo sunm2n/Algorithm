41e6187d1e48b97a3a36aa3f3062f2d2850a7246
LEE SUN MIN <134378502+sunm2n@users.noreply.github.com>
2025-08-01 19:44:45 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 172 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int L = sc.nextInt();
+
+        if(L % 5 == 0) {
+            System.out.println(L /5);
+        } else {
+            System.out.println((L/5) + 1);
+        }
+    }
+}
