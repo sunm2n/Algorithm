a6e2e61d518ddc23179bda280da4b044fda96336
LEE SUN MIN <134378502+sunm2n@users.noreply.github.com>
2025-10-13 15:37:04 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int service = n / 10;
+        answer = (n * 12000) + ((k - service) * 2000);
+        return answer;
+    }
+}
