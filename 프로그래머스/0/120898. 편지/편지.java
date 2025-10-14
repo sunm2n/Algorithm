42c6c8f5e7a33e694a563db053be4f3f198f0ef1
LEE SUN MIN <134378502+sunm2n@users.noreply.github.com>
2025-10-14 10:09:52 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        
+        int text_size = 2 * message.length();
+        
+        int answer = text_size;
+        return answer;
+    }
+}
