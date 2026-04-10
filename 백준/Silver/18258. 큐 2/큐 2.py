d35bfa17f7c1f163a72272943e9ec2ebb58300d8
LEE SUN MIN <sunmin8410@mju.ac.kr>
2026-04-10 17:28:35 +0900

    [Silver IV] Title: 큐 2, Time: 1616 ms, Memory: 144256 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N = int(input())
+
+queue = deque()
+
+for _ in range(N):
+    command = list(input().split())
+
+    if command[0] == "push":
+        queue.append(command[1])
+
+    elif command[0] == "pop":
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+
+    elif command[0] == "size":
+        print(len(queue))
+
+    elif command[0] == "empty":
+        if queue:
+            print(0)
+        else:
+            print(1)
+
+    elif command[0] == "front":
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+
+    elif command[0] == "back":
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
