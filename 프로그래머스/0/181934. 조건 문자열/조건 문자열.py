54c402a4c0506fc28a84a92afd77d956ec546611
LEE SUN MIN <sunmin8410@mju.ac.kr>
2026-04-17 21:41:25 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,28 @@
+def solution(ineq, eq, n, m):
+    answer = -1
+    
+    if ineq == ">" and eq == "=":
+        if n >= m:
+            answer = 1
+        else:
+            answer = 0
+    
+    if ineq == "<" and eq == "=":
+        if n <= m:
+            answer = 1
+        else:
+            answer = 0
+    
+    if ineq == ">" and eq == "!":
+        if n > m:
+            answer = 1
+        else:
+            answer = 0
+            
+    if ineq == "<" and eq == "!":
+        if n < m:
+            answer = 1
+        else:
+            answer = 0
+    
+    return answer
