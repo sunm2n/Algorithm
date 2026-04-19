3916a7605ec7b4a8cd9c2e96f6a2df46e0688f6e
LEE SUN MIN <sunmin8410@mju.ac.kr>
2026-04-19 18:10:45 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 6.35 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,23 +1,3 @@
-def count(n):
-    count = 0
-    i = 1
-    while i * i <= n:
-        count += 1
-        if i * i != n:
-            count += 1
-        i += 1
-    
-    return count
-    
-        
 def solution(left, right):
     answer = 0
-    
-    for i in range(left, right + 1):
-        count_divisor = count(i)
-        if count_divisor % 2 == 0:
-            answer += i
-        else:
-            answer -= i
-    
     return answer
