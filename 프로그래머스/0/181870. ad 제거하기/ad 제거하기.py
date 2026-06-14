1f2d17c0df935a37e36deede1babcf0f4707aa48
LEE SUN MIN <sunmin8410@mju.ac.kr>
2026-06-15 03:11:07 +0900

    [level 0] Title: ad 제거하기, Time: 0.06 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -0,0 +1,10 @@
+def solution(strArr):
+    answer = []
+    
+    for i in range(len(strArr)):
+        if "ad" in strArr[i]:
+            continue
+        else:
+            answer.append(strArr[i])
+    
+    return answer
