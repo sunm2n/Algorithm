9a5c9e65f77a449b634a358bfbef3afabbbae343
LEE SUN MIN <sunmin8410@mju.ac.kr>
2026-07-14 15:20:57 +0900

    [level 0] Title: A 강조하기, Time: 0.02 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = myString.toLowerCase();
+        
+        answer = answer.replace("a", "A");
+        
+        return answer;
+    }
+}
